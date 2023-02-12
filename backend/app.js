2f85cc4ddeb6d51f65aff2ed4003629518346d94
tbensheimer <104734721+tbensheimer@users.noreply.github.com>
2023-02-12 10:46:46 -0500

    Added storeRoutes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const storeRoutes = require("./routes/storeRoutes");
 
 const app = express();
 
@@ -15,3 +16,5 @@ mongoose.connect(process.env.MONGO_URI)
 .catch((error) => {
 console.log(error);
 })
+
+app.use('/store', storeRoutes);
