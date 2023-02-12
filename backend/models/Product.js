425a8375aef69c1a09a840bb2fd5cb3cbb7d5022
tbensheimer <104734721+tbensheimer@users.noreply.github.com>
2023-02-12 10:50:28 -0500

    Create Product.js

---
+++ backend/models/Product.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    price_id: {
+        type: String,
+        required: false,
+    },
+    protein: {
+        type: Number,
+        required: true,
+    },
+    fat: {
+        type: Number,
+        required: true,
+    },
+    carbs: {
+        type: Number,
+        required: true,
+    },
+    salt: {
+        type: Number,
+        required: true
+    },
+    storage: {
+        type: String,
+        required: true,
+    }
+})
+
+module.exports = mongoose.model('Product', productSchema);
