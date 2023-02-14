d7c7cc45cdae00413e91b9340902f48efa5e21f8
tbensheimer <104734721+tbensheimer@users.noreply.github.com>
2023-02-13 19:18:32 -0500

    Create Products.js

---
+++ frontend/src/pages/Products.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import Product from "../components/Product";
+import Loader from "../components/Loader";
+
+export default function Products(props) {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    
+      const fetchProducts = async () => {
+        const response = await fetch("/store/products");
+        const data = await response.json();
+
+        if(response.ok) {
+            setProducts(data.products);
+            setIsLoading(false);
+        }
+        else {
+            console.log("error in getting products");
+            setIsLoading(false);
+        }
+      }
+      fetchProducts();
+  }, []);
+
+  return (
+    <div className="products-layout">
+      <h1>Products</h1>
+      <p>Take a look at our products</p>
+      <div className="products-grid">
+      {isLoading && <Loader />}
+        {products && products.map((product) => {
+          return (
+            <Product
+              key={product._id}
+              details={product}
+            ></Product>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
