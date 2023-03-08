3aea3c6b7fda12cce269a5283d249491b41cc7e1
tbensheimer <104734721+tbensheimer@users.noreply.github.com>
2023-03-07 22:01:51 -0500

    Set up user authentication/authorization

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const storeRoutes = require("./routes/storeRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -18,3 +19,5 @@ console.log(error);
 })
 
 app.use('/store', storeRoutes);
+
+app.use('/user', userRoutes);
