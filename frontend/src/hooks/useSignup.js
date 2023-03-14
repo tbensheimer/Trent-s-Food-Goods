8152c9f2603e1be07ba31b1193141e983c9ab5cd
tbensheimer <104734721+tbensheimer@users.noreply.github.com>
2023-03-14 18:46:54 -0400

    Create useSignup.js

---
+++ frontend/src/hooks/useSignup.js
@@ -0,0 +1,37 @@
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {login} from "../redux/store";
+
+export default function useSignup() {
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState("");
+const dispatch = useDispatch();
+
+const Signup = async(email, password) => {
+    setLoading(true);
+    setError(null);
+
+    const response = await fetch("/user/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email, password})
+    });
+    const data = await response.json();
+
+    if(!response.ok) {
+        setError(data.error);
+        setLoading(false);
+    }
+
+    if(response.ok) {
+        setLoading(false);
+        localStorage.setItem("user", JSON.stringify(data));
+        dispatch(login(data));
+    }
+}
+
+return {error, loading, Signup}
+
+}
